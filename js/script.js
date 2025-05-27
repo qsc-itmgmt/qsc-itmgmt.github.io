1fa168b4b739212bcab64b74ed8e9666a9dd6d34
qsc-itmgmt <it-management@qsc-systems.com>
2025-05-27 13:53:47 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,16 +3,14 @@ function getLinkParam() {
     return urlParams.get("link");
 }
 
-const code = getLinkParam();
+var code = getLinkParam();
 
-if (code) {
-    const fullUrl = `https://player.vimeo.com/video/${code}?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479`;
-    const decodedUrl = decodeURIComponent(fullUrl);
-
-    const iframe = document.getElementById("video-frame");
-    iframe.src = decodedUrl;
-
-    document.getElementById("video-container").style.display = "flex";
-} else {
-    document.getElementById("video-info").innerHTML = "<p>Kein 'link'-Parameter in der URL gefunden.</p>";
+if (!code) {
+    code = "1";
 }
+
+const fullUrl = `https://player.vimeo.com/video/${code}?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479`;
+const decodedUrl = decodeURIComponent(fullUrl);
+const iframe = document.getElementById("video-frame");
+iframe.src = decodedUrl;
+document.getElementById("video-container").style.display = "flex";
